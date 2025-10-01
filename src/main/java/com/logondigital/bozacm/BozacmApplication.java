71a032cbcb8e649f654a2fbe5ac89522c47420e5
YouyouGlody <Youyoustyve@gmail.com>
2025-10-01 16:56:24 +0100

    Ajout des dossiers .idea et target

---
+++ src/main/java/com/logondigital/bozacm/BozacmApplication.java
@@ -0,0 +1,13 @@
+package com.logondigital.bozacm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BozacmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BozacmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logondigital/bozacm/BozacmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logondigital.bozacm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BozacmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
