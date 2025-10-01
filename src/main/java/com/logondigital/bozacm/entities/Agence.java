2cf4bd2a1be2e2247628053a7190989e4a2c42ca
jadone237 <martinfouepi@gmail.com>
2025-10-01 19:40:47 +0200

    creation de l'entité Agence

---
+++ src/main/java/com/logondigital/bozacm/entities/Agence.java
@@ -0,0 +1,49 @@
+package com.logondigital.bozacm.entities;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "agences")
+public class Agence {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nom;
+    private String adresse;
+    private String email;
+    private String telephone;
+    private Date createdAt;
+    @Temporal(TemporalType.DATE)
+    private Date updatedAt;
+    @OneToMany(mappedBy = "agence")
+
+
+
+    public void setName(String name) {
+        this.nom= name;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+
+}
+
