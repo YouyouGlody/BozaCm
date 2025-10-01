31ecb6fc8e356e270a3a33fc019105995e12314c
jadone237 <martinfouepi@gmail.com>
2025-10-01 21:19:58 +0200

    Creation TrajetSevice

---
+++ src/main/java/com/logondigital/bozacm/service/trajet/TrajetService.java
@@ -0,0 +1,19 @@
+package com.logondigital.bozacm.service.trajet;
+
+import com.logondigital.bozacm.entities.Trajet;
+
+import java.util.List;
+
+public interface TrajetService {
+    String createTrajet(Trajet trajet);
+
+    List<Trajet> getTrajets();
+
+    Trajet getTrajetById(Integer trajetId);
+
+    String updateTrajet(Integer trajetId, Trajet trajet);
+
+    String deleteTrajet(Integer trajetId);
+
+    void CreateTrajet(Trajet trajet);
+}
