4a2427fd9f80ab72b65d2e27a32ebbc61b76d01b
jadone237 <martinfouepi@gmail.com>
2025-10-01 19:45:38 +0200

    creation de l'entité Offre

---
+++ src/main/java/com/logondigital/bozacm/entities/Offre.java
@@ -0,0 +1,58 @@
+package com.logondigital.bozacm.entities;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "offres")
+public class Offre {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String titre;
+    private String description;
+    private Double prix;
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+    @Temporal(TemporalType.DATE)
+    private Date updatedAt;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateDepart;
+
+    @ManyToOne
+    private Agence agence;
+
+   
+    public String getName() {
+        return titre;
+    }
+
+    public void setName(String name) {
+        this.titre= name;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+
+
+    }
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+}
+
