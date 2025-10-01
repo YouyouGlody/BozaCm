a5665e4410658a9cf6343a4e77e5e90a61db0efa
YouyouGlody <Youyoustyve@gmail.com>
2025-10-01 19:33:55 +0100

    Création et mise en place des annotations pour l'entitée Billet

--- src/main/java/com/logondigital/bozacm/entities/Billet.java
+++ src/main/java/com/logondigital/bozacm/entities/Billet.java
@@ -1,4 +1,23 @@
 package com.logondigital.bozacm.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "billets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Billet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idBillet;
+    private String numeroBillet;
+    private  String qrcodeUrl;
+    private LocalDateTime dateEmission;
 }
