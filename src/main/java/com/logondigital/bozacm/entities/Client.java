c6bfd55051445dd8c21f44ace53d74d8a6c0a79a
YouyouGlody <Youyoustyve@gmail.com>
2025-10-01 19:15:51 +0100

    annotations pour l'entité client

--- src/main/java/com/logondigital/bozacm/entities/Client.java
+++ src/main/java/com/logondigital/bozacm/entities/Client.java
@@ -1,4 +1,26 @@
 package com.logondigital.bozacm.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "clients")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idClient;
+    private String nom;
+    private String prenom;
+    private Integer numeroTelephone;
+    private String email;
+    private String adresse;
+    private Date createdAt;
+    private Date updatedAt;
+
 }
