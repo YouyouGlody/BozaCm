6d2b8f557e5cbfb94450b6865e8a244d69d4f28e
jadone237 <martinfouepi@gmail.com>
2025-10-06 02:11:37 +0200

    configuration des exception dans OffreServvice

--- src/main/java/com/logondigital/bozacm/service/Offre/OffreServiceImpl.java
+++ src/main/java/com/logondigital/bozacm/service/Offre/OffreServiceImpl.java
@@ -1,6 +1,9 @@
 package com.logondigital.bozacm.service.Offre;
 
 import com.logondigital.bozacm.entities.Offre;
+import com.logondigital.bozacm.exception.DatabaseException;
+import com.logondigital.bozacm.exception.InvalidRequestException;
+import com.logondigital.bozacm.exception.ResourceNotFoundException;
 import com.logondigital.bozacm.repository.OffreRepo;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +20,26 @@ public class OffreServiceImpl implements OffreService {
 
     @Override
     public String createOffre(Offre offre) {
+        if (offre.getTitre() == null || offre.getTitre().isBlank()) {
+            throw new InvalidRequestException("Le titre de l'offre est obligatoire !");
+        }
+        if (offre.getPrix() == null || offre.getPrix() <= 0) {
+            throw new InvalidRequestException("Le prix doit être supérieur à 0 !");
+        }
+
+        // Vérifier que l’agence existe
+        if (offre.getAgence() == null || offre.getAgence().getId() == null) {
+            throw new InvalidRequestException("Une offre doit être liée à une agence !");
+        }
+
+        // Vérifier que le trajet existe
+        if (offre.getTrajet() == null || offre.getTrajet().getId() == null) {
+            throw new InvalidRequestException("Une offre doit être liée à un trajet !");
+        }
+
         offre.setCreatedAt(new Date());
-        this.offreRepo.save(offre);
-        return "Offre created";
+       this.offreRepo.save(offre);
+        return "Offre créée avec succès !";
     }
 
     @Override
@@ -29,21 +49,31 @@ public class OffreServiceImpl implements OffreService {
 
     @Override
     public Offre getOffreById(Integer offreId) {
-        return this.offreRepo.findById(offreId).get();
+        return this.offreRepo.findById(offreId) .orElseThrow(() -> new ResourceNotFoundException("L’offre introuvable essayer un autre id."));
     }
 
     @Override
     public String updateOffre(Integer offreId, Offre offre) {
-        Offre offreToUpdate = this.offreRepo.findById(offreId).get();
-        offreToUpdate.setName(offre.getName());
-        offreToUpdate.setUpdatedAt(new Date());
-        this.offreRepo.saveAndFlush(offreToUpdate);
+       try {
 
-        return "Offre updated with succes";
+           Offre offreToUpdate = this.offreRepo.findById(offreId).orElseThrow(() -> new ResourceNotFoundException("L’offre avec cette id n’existe pas."));
+
+           if (offre.getPrix() <= 0) {
+               throw new InvalidRequestException("Le prix doit être supérieur à 0.");
+           }
+           offreToUpdate.setName(offre.getName());
+           offreToUpdate.setUpdatedAt(new Date());
+           this.offreRepo.saveAndFlush(offreToUpdate);
+
+           return "Offre updated with succes";
+       }catch (Exception e) {
+           throw new DatabaseException("Erreur lors mise a jour de l'offre: " + e.getMessage());
+       }
     }
 
     @Override
     public String deleteOffre(Integer offreId) {
+        Offre offreToDelete = this.offreRepo.findById(offreId).orElseThrow(() -> new ResourceNotFoundException("L’offre cette n’existe pas essaye un autre."));
         this.offreRepo.deleteById(offreId);
         return "";
     }
