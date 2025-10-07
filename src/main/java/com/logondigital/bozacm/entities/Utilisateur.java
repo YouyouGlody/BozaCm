686231cea9e3edc13f86bf07d759c291ec434171
emmanuellebnz <benzoemmanuelle@gmail.com>
2025-10-07 10:40:08 +0100

    Creation de l'entite utilisateur

---
+++ src/main/java/com/logondigital/bozacm/entities/Utilisateur.java
@@ -0,0 +1,31 @@
+package com.logondigital.bozacm.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Setter
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "utilisateurs")
+public class Utilisateur {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Integer utilisateurId;
+    private String nom ;
+    private String prenom ;
+    private String email ;
+    private String motDePasse ;
+    private String telephone ;
+    private String photoProfil ;
+    private LocalDate dateCreation ;
+    private boolean actif ;
+
+
+
+}
