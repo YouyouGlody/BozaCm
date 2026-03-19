ad8a14c6d7bbbee751033a7fbd9b875da97ec466
wafo vianey <wafodarius2@gmail.com>
2026-03-19 20:30:05 +0100

    sixieme sauvegarde

---
+++ src/main/java/com/logondigital/bozacm/DTO/EtapeReq.java
@@ -0,0 +1,78 @@
+package com.logondigital.bozacm.DTO;
+
+import com.example.gestion_trajets.enums.TypeEtape;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+public class EtapeReq {
+    @NotEmpty(message = "name is required")
+    private String nomEtape;
+    private String ville;
+    @NotEmpty(message = "name is required")
+    private String pays;
+    private Integer dureeArret;
+    @NotNull(message = "Le type d'étape est obligatoire")
+    private TypeEtape typeEtape;
+    private Integer ordre;
+
+    public EtapeReq(TypeEtape typeEtape, Integer dureeArret, String pays, String ville, String nomEtape, Integer ordre, Date dateCreation, Date dateModification) {
+        this.typeEtape = typeEtape;
+        this.dureeArret = dureeArret;
+        this.pays = pays;
+        this.ville = ville;
+        this.nomEtape = nomEtape;
+        this.ordre = ordre;
+    }
+
+    public EtapeReq() {}
+
+    public String getNomEtape() {
+        return nomEtape;
+    }
+
+    public void setNomEtape(String nomEtape) {
+        this.nomEtape = nomEtape;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public Integer getDureeArret() {
+        return dureeArret;
+    }
+
+    public void setDureeArret(Integer dureeArret) {
+        this.dureeArret = dureeArret;
+    }
+
+    public TypeEtape getTypeEtape() {
+        return typeEtape;
+    }
+
+    public void setTypeEtape(TypeEtape typeEtape) {
+        this.typeEtape = typeEtape;
+    }
+
+    public Integer getOrdre() {
+        return ordre;
+    }
+
+    public void setOrdre(Integer ordre) {
+        this.ordre = ordre;
+    }
+}
