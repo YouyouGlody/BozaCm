5afada87a0ee6cfa6ef034580911dadbbddbb913
wafo vianey <wafodarius2@gmail.com>
2026-03-19 20:30:44 +0100

    septieme sauvegarde

---
+++ src/main/java/com/logondigital/bozacm/DTO/EtapeResp.java
@@ -0,0 +1,82 @@
+package com.logondigital.bozacm.DTO;
+
+
+import com.example.gestion_trajets.enums.TypeEtape;
+
+public class EtapeResp {
+    private Integer id;
+    private String nomEtape;
+    private String ville;
+    private String pays;
+    private Integer dureeArret;
+    private TypeEtape typeEtape;
+    private Integer ordre;
+
+    public EtapeResp(Integer id, String nomEtape, String ville, String pays, Integer dureeArret, TypeEtape typeEtape, Integer ordre) {
+        this.id = id;
+        this.nomEtape = nomEtape;
+        this.ville = ville;
+        this.pays = pays;
+        this.dureeArret = dureeArret;
+        this.typeEtape = typeEtape;
+        this.ordre = ordre;
+    }
+
+    public EtapeResp() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomEtape() {
+        return nomEtape;
+    }
+
+    public void setNomEtape(String nomEtape) {
+        this.nomEtape = nomEtape;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public Integer getDureeArret() {
+        return dureeArret;
+    }
+
+    public void setDureeArret(Integer dureeArret) {
+        this.dureeArret = dureeArret;
+    }
+
+    public TypeEtape getTypeEtape() {
+        return typeEtape;
+    }
+
+    public void setTypeEtape(TypeEtape typeEtape) {
+        this.typeEtape = typeEtape;
+    }
+
+    public Integer getOrdre() {
+        return ordre;
+    }
+
+    public void setOrdre(Integer ordre) {
+        this.ordre = ordre;
+    }
+}
