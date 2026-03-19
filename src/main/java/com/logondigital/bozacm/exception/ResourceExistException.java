4a33039ce44d152e7a6cbd61e4ca2ff54742548d
wafo vianey <wafodarius2@gmail.com>
2026-03-19 20:27:13 +0100

    quatrieme sauvegarde

---
+++ src/main/java/com/logondigital/bozacm/exception/ResourceExistException.java
@@ -0,0 +1,7 @@
+package com.logondigital.bozacm.exception;
+
+public class ResourceExistException extends RuntimeException {
+    public ResourceExistException(String message) {
+        super(message);
+    }
+}
